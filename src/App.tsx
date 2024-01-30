62aac1bf26501acc6802ed2d7f4781761be715b9
Sarutha Vignesh <emailsaruthavignesh@gmail.com>
2024-01-30 08:42:53 +0530

    Sample change commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Footer } from './components/Footer'
 import { Cart } from './components/Cart'
 import { Products } from './components/Products'
-import {Header } from './components/Header'
+import { Header } from './components/Header'
 import './app.module.scss'
-import React from 'react'
 
 function App() {
   return (
